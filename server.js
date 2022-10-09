9f5705813b6994f7b427a2ba3e63a8bc46b2f199
محمود يحيي شوقي عبدالخالق <14712020101567@stud.cu.edu.eg>
2022-10-09 22:33:39 +0200

    sever-side ended with resposive

--- server.js
+++ server.js
@@ -17,6 +17,23 @@ app.use(cors());
 //link with the clint-code
 app.use(express.static("./website"));
 
+let projectData = {};
+
+//GEt-root
+app.get("/getData", (req, res) => {
+  res.send(projectData);
+  console.log(projectData);
+  console.log(5);
+});
+
+//POST-root
+app.post("/addData", (req, res) => {
+  const newData = req.body;
+  projectData.date = newData.date;
+  projectData.temperature = (Number(newData.temperature) - 273).toFixed(2);
+  projectData.userRes = newData.userRes;
+  //   console.log(projectData);
+});
 
 app.listen(port, () => {
   console.log(`server is porting on ${port}`);
---
+++ website/app.js
@@ -0,0 +1,79 @@
+//Global variables
+const baseUrl = "http://api.openweathermap.org/data/2.5/weather?zip=";
+const personalKey = "&appid=2ad070d9e09ca7a503ec39544c161e29";
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate =
+  Number(d.getMonth()) + 1 + ":" + d.getDate() + ":" + d.getFullYear();
+
+const getData = async (baseUrl, zipCode = "", personalKey = "") => {
+  const response = await fetch(baseUrl + zipCode + personalKey);
+
+  try {
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.log("Error", err);
+  }
+};
+
+//post data
+const postData = async (path, data) => {
+  const response = await fetch(path, {
+    method: "post",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  try {
+    const resData = await response.json();
+    return resData;
+  } catch (err) {
+    console.log("Error", err);
+  }
+};
+
+const performAction = () => {
+  // inputs
+  const zipCode = document.getElementById("zip").value;
+  const userResponse = document.getElementById("feel").value;
+
+  getData(baseUrl, zipCode, personalKey).then((data) => {
+    // post data to my server
+    postData("/addData", {
+      date: newDate,
+      temperature: data.main.temp,
+      userRes: userResponse,
+    });
+    updateUI();
+  });
+
+  document.getElementById("zip").value = "";
+  document.getElementById("feel").value = "";
+};
+
+// on clicking generate button
+document.getElementById("generate").addEventListener("click", performAction);
+
+//get data from server
+
+const updateUI = async () => {
+  const response = await fetch("/getData");
+  try {
+    const data = await response.json();
+
+    //assign data to weather holders
+    document.querySelector(".date").textContent = data.date;
+    document.querySelector(".temp").innerHTML =
+      data.temperature + " <span><sup>o</sup>C</span>";
+    document.querySelector(".content").textContent = data.userRes;
+    
+  } catch (err) {
+    console.log("Error", err);
+  }
+ 
+
+};
